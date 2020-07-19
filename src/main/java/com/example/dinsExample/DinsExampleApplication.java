90d7421419e7bd545908864c3cbda93a11105bbf
toktobubu <torolbaeva@beeline.kz>
2020-07-19 16:33:49 +0600

    First commit

---
+++ src/main/java/com/example/dinsExample/DinsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dinsExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinsExampleApplication.class, args);
+	}
+
+}
